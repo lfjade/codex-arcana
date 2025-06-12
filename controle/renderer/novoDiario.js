bb493f4ef01dd187bc170f09c52f60e448d3e5c5
jade <fernandes.l.jade@gmail.com>
2025-06-12 04:01:49 -0300

    development in novoDiario.js file

--- controle/renderer/novoDiario.js
+++ controle/renderer/novoDiario.js
@@ -1,24 +1,51 @@
 export default async function novoDiario(){
     if (!window.location.pathname.includes('novoDiario.html')) return
-    const params = new URLSearchParams(window.location.search)
-        const nomeArquivo = params.get('arquivo')
-    
-        if (!nomeArquivo){
-            console.error('Nenhum arquivo especificado na URL.')
-            return
-        }
 
-    try{
-        const conteudo = await window.electron.invoke('ler-diario', nomeArquivo)
+    const titulo = document.getElementById('titulo')
+    const conteudo = document.getElementById('ritualistica')
+    const btnRegistrar = document.getElementById('registrar')
 
-        if (conteudo.erro){
-                console.error(conteudo.erro)
-            } else {
-                const titulo = nomeArquivo.replace(/\.txt$/, '')
-                document.getElementById('titulo-diario').value = titulo
+    // colocar depois de registrar
+    if (btnRegistrar){
+        const novoBtn=btnRegistrar.cloneNode(true)
+        btnRegistrar.replaceWith(novoBtn)
+        novoBtn.addEventListener('click', async () =>{
+            if (!titulo) alert ("Título não pode ser vazio.")
+            if (!conteudo) alert ("Conteúdo não pode ser vazio")
+
+            try {
+                window.api.salvarDiario( titulo, conteudo)
+                alert("Diário salvo!")
+            } catch (erro){
+                alert("Erro ao salvar diário.")
+                console.error(erro)
             }
-    } catch (erro) {
-        console.error('Erro inesperado: ', erro)
+
+
+        })
+
     }
+
+
+    // const params = new URLSearchParams(window.location.search)
+    //     const nomeArquivo = params.get('arquivo')
+    
+    //     if (!nomeArquivo){
+    //         console.error('Nenhum arquivo especificado na URL.')
+    //         return
+    //     }
+
+    // try{
+    //     const conteudo = await window.electron.invoke('ler-diario', nomeArquivo)
+
+    //     if (conteudo.erro){
+    //             console.error(conteudo.erro)
+    //         } else {
+    //             const titulo = nomeArquivo.replace(/\.txt$/, '')
+    //             document.getElementById('titulo-diario').value = titulo
+    //         }
+    // } catch (erro) {
+    //     console.error('Erro inesperado: ', erro)
+    // }
 
 }
